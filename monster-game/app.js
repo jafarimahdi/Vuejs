eefe168a19686a683133bf02015c0b03eb353bef
jafarimahdi <mahdijafarii.mj@gmail.com>
2020-07-10 08:02:13 +0200

    monsterGame

--- monster-game/app.js
+++ monster-game/app.js
@@ -1,8 +1,61 @@
-// Put this in the script section in JSFiddle
-// In a local setup, you need to merge this and the index.html file into one file
 new Vue({
-	el: '#app',
-	data: {
-		show: true
-	}
-})
+    el:"#app",
+    data:{
+        playerHealth:100,
+        monsterHealth: 100,
+        gameIsRunning:false
+    },
+    methods:{
+        startGame: function(){
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack:function(){ 
+            // we got the random number between 3 and 10 and use the math.max and calculate the damage 
+            this.monsterHealth -= this.calculateDamage(3,10); 
+              
+            if (this.checkWin()){
+               return
+           }
+            // monster damage for the player and condition for who lost  
+            this.playerHealth -= this.calculateDamage(5,12);
+            
+             this.checkWin();    
+        },
+        specialAttack:function(){
+            
+        },
+        heal:function(){
+
+        },
+        giveUp:function(){
+
+        },
+        // make the separate function for the each part 
+
+         calculateDamage: function(min, max){
+            return  Math.max(Math.floor(Math.random()* max) + 1, min);
+         },
+
+        checkWin:function(){
+             if(this.monsterHealth <= 0){
+                   if(confirm('You won! New Game?')){
+                       this.startGame();
+                   }else{
+                       this.gameIsRunning = false;
+                   }
+                   return true;
+             })else if(this.playerHealth <= 0){
+                 if(confirm('You lost! New Game'))
+                    this.startGame();
+                 else{
+                     this.gameIsRunning = false;
+                 }return true;
+
+             }
+             return false;
+         }
+            
+    }
+});
