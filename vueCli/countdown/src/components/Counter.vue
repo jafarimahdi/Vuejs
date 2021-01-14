562b1e2af9ba7ee7ffc44af1163228eb71c36949
jafarimahdi <mahdijafarii.mj@gmail.com>
2021-01-14 17:00:48 +0100

    countDown

---
+++ vueCli/countdown/src/components/Counter.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Hell from the counter</h1>
+    <h2>add the secound line from the counter</h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style></style>
---
+++ vueCli/countdown/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>hello form the home</h3>
+    
+    <counter />
+  </div>
+</template>
+
+<script>
+import counter from "@/components/Counter.vue";
+
+export default {
+  components: {
+    counter
+  },
+};
+</script>
+
+<style scoped></style>
